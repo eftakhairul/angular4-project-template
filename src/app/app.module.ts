ff89ca2eb4c966a9c976979468dc620973314821
Eftakhairul Islam <eftakhairul@gmail.com>
2017-05-21 15:26:39 -0400

    router implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; 
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-details.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },  
+  { path: 'heroes',     component: HeroesComponent }
+];
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,  
+                  FormsModule, 
+                  RouterModule.forRoot(routes)
+                  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
