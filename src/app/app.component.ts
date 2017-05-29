d6a5747ede089d5167115a3bb6674ce1d9f4e824
Eftakhairul Islam <eftakhairul@gmail.com>
2017-05-28 23:20:16 -0400

    Extranal HTTP service is implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Component} from '@angular/core';
              <nav>
                 <a routerLink="/dashboard">Dashboard</a>
                 <a routerLink="/heroes">Heroes</a>
+                <a routerLink="/github">Github</a>
              </nav>
              <router-outlet></router-outlet>`,
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,29 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; 
 import { RouterModule, Routes } from '@angular/router';
-
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-details.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+import { GithubComponent } from './github.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },  
-  { path: 'heroes',     component: HeroesComponent }
+  { path: 'heroes',     component: HeroesComponent },
+  { path: 'github',     component: GithubComponent }
 ];
 
 
 @NgModule({
   imports:      [ BrowserModule,  
                   FormsModule, 
-                  RouterModule.forRoot(routes)
-                  ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+                  RouterModule.forRoot(routes, {useHash: true}),
+                  HttpModule,
+                  JsonpModule],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent, GithubComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
